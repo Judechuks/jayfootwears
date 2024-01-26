8cd319754ff5c8a8a6e3af14c7e40de29cdebd25
Jude Chuks <judechuks26@gmail.com>
2024-01-26 16:16:20 +0100

    wrong import serviceCard instead of ServiceCard

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ServiceCard from "../components/serviceCard";
+import ServiceCard from "../components/ServiceCard";
 import { services } from "../constants";
 
 const Services = () => {
